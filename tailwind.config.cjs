ce85c0f8ca6199a1d1ccd20bdf15ca0df2a6cc2a
Lucy Tibbetts <lucilletibbetts@gmail.com>
2022-10-03 16:57:51 -0700

    start

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,9 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'mono': ['Courier New', 'Courier', 'mono']
+    }
   },
   plugins: [],
 }
