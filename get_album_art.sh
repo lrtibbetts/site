605a5cc40b2c04e8357de211760b613ab37bafc0
Lucy Tibbetts <lucilletibbetts@gmail.com>
2022-10-06 15:47:21 -0700

    fix json

--- get_album_art.sh
+++ get_album_art.sh
@@ -3,5 +3,5 @@
 # Through The Years playlist id: 4CVjYLoQGb4AVdagZvSS2r
 
 curl -X "GET" "https://api.spotify.com/v1/playlists/4CVjYLoQGb4AVdagZvSS2r" -H "Accept: application/json" \
--H "Content-Type: application/json" -H "Authorization: Bearer <TOKEN>" \
-| jq '.tracks.items[].track.album | { name: .name, date: .release_date, image: .images[0].url }' > result.txt
+-H "Content-Type: application/json" -H "Authorization: Bearer BQCxkxjnaH7nT1rDJw-uHfnmDyAU4xAKwkA1_GD2HvB2IjLhPUcdZIbJXiKCWDQQczggjNSWCaivwmzZIiNW41kUFLKVSUCfKduqVE-2Pw0d0vPA4-A49pPOWHNXyTtvuuI-9Glx8ToYVXmeHw8hnxkio2qjNpiJBME2plgHFpXOyOdX" \
+| jq '[.tracks.items[].track.album | { name: .name, date: .release_date, image: .images[0].url }]' > albums.json
