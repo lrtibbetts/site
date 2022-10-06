4ede68ac08d40a504938310349443f5eb461d5d5
Lucy Tibbetts <lucilletibbetts@gmail.com>
2022-10-06 15:19:07 -0700

    spotify

---
+++ get_album_art.sh
@@ -0,0 +1,7 @@
+
+# to get fresh oauth token: https://developer.spotify.com/console/get-playlist/
+# Through The Years playlist id: 4CVjYLoQGb4AVdagZvSS2r
+
+curl -X "GET" "https://api.spotify.com/v1/playlists/4CVjYLoQGb4AVdagZvSS2r" -H "Accept: application/json" \
+-H "Content-Type: application/json" -H "Authorization: Bearer <TOKEN>" \
+| jq '.tracks.items[].track.album | { name: .name, date: .release_date, image: .images[0].url }' > result.txt
