0893e28d16a3152d7ef86906fbc7900f9166945c
Lucy Tibbetts <lucilletibbetts@gmail.com>
2022-10-11 09:39:29 -0700

    center

--- src/albums.ts
+++ src/albums.ts
@@ -9,12 +9,12 @@ export function getAlbums() {
         let img = document.createElement("img");
         img.src = "../" + album.file;
         //img.src = album.image;
-        img.className = "hover:opacity-30"
+        img.className = "hover:opacity-30 m-auto"
         div.appendChild(img);
 
         let text = document.createElement("p");
         text.innerHTML = album.trimmed_name + ", " + album.date.substring(0,4);
-        text.className = "text-black bg-white rounded-sm p-1 font-bold xl:text-lg absolute text-center album-text"
+        text.className = "text-black bg-white rounded-sm p-1 font-bold xl:text-lg text-center album-text"
         div.appendChild(text);
 
         albumsContainer?.appendChild(div);
